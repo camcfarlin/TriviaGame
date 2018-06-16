4ad23b94781591249fe7b11e68cca952bb968418
Cesar McFarlin <cesarmcfarlin@cesars-mbp.lan>
2018-06-16 17:20:14 -0600

    intial setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+// PsudeoCode
+// declare variables 
+    // Timer
+    // QuestionLeft
+    // Choice 1-4
+    // rightAnswer
+    // wrongAnswer
+    // unanswered
+
+
+// JQUERY ID
+    // question
+    // timer 
+    // .choiceOne
+    // .choiceTwo
+    // .choiceThree
+    // .choiceFour
+    
+// Arrays
+    // Questions 1 - 4 choices
+    // Q2 - 4 Choices
+    // Q3 - 4 Choices
+    // Q4 - 4 Choices    
+    // Q5 - 4 Choices
+    // Q6 - 4 Choices
+
+// event listener - click /select
+    //if correct
+        //rightAnswer ++
+        //ackoweldge correct answer
+        //clycle next question
+    //if else wrong 
+        //wrongAnswer++
+        //diplay correct answer
+        //cycle next question
+    // else
+        // if timer goes to 0
+            //unanswered ++
+                //diplay correct answer
+                //cycle next question
+        
