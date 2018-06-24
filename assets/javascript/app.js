f7723b068e91b5591081c3131edfa9a0eb97f867
Cesar McFarlin <cesarmcfarlin@Cesars-MacBook-Pro.local>
2018-06-24 15:37:25 -0600

    adjustment - ajax/json notes
    added notes to come back to adjust to allow random trvia api pull

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,58 @@
 window.onload = function(){
 	$(".answer").on("click", function(){
 		game.selectedAnswer = $(this).text();
-		game.reveal();
+		reveal();
 	});
 };
+
+//ajax/json notes- 
+// var answerAnswer = [];
+// var myObj = [];
+// var randomAnswer = [];
+// // JSON/AJAX ITEMS 
+
+// var category = "15";
+// var queryURL = "https://opentdb.com/api.php?amount=10&category="+category;
+
+// // $.get(queryURL,function(request,response){
+// // 	console.log(response);
+	
+// // }).then(function(data){
+// // 	console.log(data);
+// // });
+
+
+// $.ajax({
+// 	url: queryURL,
+// 	method: "GET"
+// 	}).then(function(response_code) {
+// 	console.log(response_code)
+// 	myObj = response_code.results;
+// 	randomAnswer = (response_code.results[0].incorrect_answers.concat(response_code.results[0].correct_answer));
+// 	// console.log(randomAnswer)
+// 	shuffle (randomAnswer);
+// 	});
+
+// function shuffle(a) {
+// 	var j, x, i;
+//     for (i = a.length - 1; i > 0; i--) {
+// 		j = Math.floor(Math.random() * (i + 1));
+// 		// console.log (i + "=i", j+ "=j",);
+// 		x = a[i];
+// 		// console.log (x+ "=x", a+ "=a", i+ "=i");
+// 		a[i] = a[j];
+// 		// console.log (a+ "=a", i+ "=i", j+ "=j");
+// 		a[j] = x;
+// 		// console.log (i, j, x, a);
+//     }
+//     // console.log(a);
+// }
+// // setTimeout(() => {
+// // }, 500);
+// // console.log (myObj[0].question);
+// // var set as objects to be called on
+
+
 // var set as objects to be called on
 var game = {
 	questions:[
@@ -12,7 +61,7 @@ var game = {
 		answers:["Press to Start", "" , "" , ""]
 	},
 	{
-		question:"What year was the first edition of Dungeons and Dragons published?",
+	question:"What year was the first edition of Dungeons and Dragons published?",
 		answers:["1974", "1983", "1969", "1982"],
 		correctAnswerIndex: 1,
 		gifImage:"assets/images/dndgif.gif"
@@ -47,94 +96,92 @@ var game = {
 		correctAnswerIndex:1,
 		gifImage:"assets/images/dragon.gif"
     }],
-    
-	numberOfQuestions:6,
-	counter: "",
-	state: 0,
-	timer: 30,
-	correctAnswers: 0,
-	incorrectAnswers: 0,
-	unanswered: 0,
-    selectedAnswer: "",
-    
-	progress:function(){
-		game.state++;
-		game.resetTimer();
-		if(game.state <= game.numberOfQuestions)
+}
+	var numberOfQuestions = 6;
+	var counter =  "";
+	var state = 0;
+	var timer = 30;
+	var correctAnswers = 0;
+	var incorrectAnswers = 0;
+	var unanswered = 0;
+    var selectedAnswer =  "";
+   
+	function progress(){
+		state++;
+		resetTimer();
+		if(state <= numberOfQuestions)
 		{
             $("#timer, #choiceOne, #choiceTwo, #choiceThree, #choiceFour").show();
-			$("#timer").text("Time Remaining: " + game.timer + " seconds");
-			game.counter = setInterval(game.count, 1000);
-			$("#question").text(game.questions[game.state].question);
-			$("#choiceOne").text(game.questions[game.state].answers[0]);
-			$("#choiceTwo").text(game.questions[game.state].answers[1]);
-			$("#choiceThree").text(game.questions[game.state].answers[2]);
-			$("#choiceFour").text(game.questions[game.state].answers[3]);
+			$("#timer").text("Time Remaining: " + timer + " seconds");
+			counter = setInterval(count, 1000);
+			$("#question").text(game.questions[state].question);
+			$("#choiceOne").text(game.questions[state].answers[0]);
+			$("#choiceTwo").text(game.questions[state].answers[1]);
+			$("#choiceThree").text(game.questions[state].answers[2]);
+			$("#choiceFour").text(game.questions[state].answers[3]);
 
 		}
 		else
 		{
-			$("#question").html('All done, here\'s how you did!<br><h3>Correct Answers: ' + game.correctAnswers+ '<br>Incorrect Answers: '+ game.incorrectAnswers +'<br>Unanswered: ' + game.unanswered + '</h3>');
+			$("#question").html('All done, here\'s how you did!<br><h3>Correct Answers: ' + correctAnswers+ '<br>Incorrect Answers: '+ incorrectAnswers +'<br>Unanswered: ' + unanswered + '</h3>');
             $("#choiceOne").show();
             $("#choiceOne").text("Start Over?");
             // need to find a way to remove div, empty div still shows :hover property
             $("#timer, #choiceTwo, #choiceThree, #choiceFour").hide();
 			//reset game
-			game.state = 0;
-			game.incorrectAnswers = 0;
-			game.correctAnswers = 0;
-			game.unanswered = 0;
+			state = 0;
+			incorrectAnswers = 0;
+			correctAnswers = 0;
+			unanswered = 0;
 		}
 
 
 
-	},
-	count:function(){
-		game.timer--;
-		$("#timer").text("Time Remaining: " + game.timer + " seconds");
-		if (game.timer == 0)
+	};
+	function count(){
+		timer--;
+		$("#timer").text("Time Remaining: " + timer + " seconds");
+		if (timer == 0)
 		 {
 		 	game.selectedAnswer = "";
-		 	game.reveal();
+		 	reveal();
 		 }
-    },
+    };
     //game time 30 secs
-	resetTimer:function(){
-		game.timer = 30;
-    },
+	function resetTimer(){
+		timer = 30;
+    };
     
     // create a page to relay to user correct or wrong answer 
     // also if time runds out
-	reveal:function(){
-		if(game.state > 0)
+	function reveal(){
+		if(state > 0)
 		{
             // if object - correctanswerindex matches users choice ++
-			if(game.questions[game.state].answers[game.questions[game.state].correctAnswerIndex] == game.selectedAnswer )
+			if(game.questions[state].answers[game.questions[state].correctAnswerIndex] == game.selectedAnswer )
 			{
 				game.correctAnswers++;
-                $("#question").html('Correct!<br><br><br><img src="'+ game.questions[game.state].gifImage +'">');
+                $("#question").html('Correct!<br><br><br><img src="'+ game.questions[state].gifImage +'">');
                 $("#timer, #choiceOne, #choiceTwo, #choiceThree, #choiceFour").hide();
             }
             // if unanswered
 			else if(game.selectedAnswer == "")
 			{
 				game.unanswered++;
-				$("#question").html('Out of Time!<br><br><h3>The correct answer was: ' + game.questions[game.state].answers[game.questions[game.state].correctAnswerIndex] +'</h3><br><img src="'+ game.questions[game.state].gifImage +'">');
+				$("#question").html('Out of Time!<br><br><h3>The correct answer was: ' + game.questions[state].answers[game.questions[state].correctAnswerIndex] +'</h3><br><img src="'+ game.questions[state].gifImage +'">');
                 $("#timer, #choiceOne, #choiceTwo, #choiceThree, #choiceFour").hide();		
             }
             // if object - correctanswerindex does not match users choice, show correct answer
 			else
 			{
 				game.incorrectAnswers++;
-				$("#question").html('Nope!<br><br><h3>The correct answer was: ' + game.questions[game.state].answers[game.questions[game.state].correctAnswerIndex]+'</h3><br><img src="'+ game.questions[game.state].gifImage +'">');
+				$("#question").html('Nope!<br><br><h3>The correct answer was: ' + game.questions[state].answers[game.questions[state].correctAnswerIndex]+'</h3><br><img src="'+ game.questions[state].gifImage +'">');
                 $("#timer, #choiceOne, #choiceTwo, #choiceThree, #choiceFour").hide();		
             }
             // clear
-			clearInterval(game.counter);
-			setTimeout(game.progress, 4000);
+			clearInterval(counter);
+			setTimeout(progress, 4000);
 		}
 		else
-			game.progress();
-	}
-};
-
+			progress();
+	};
